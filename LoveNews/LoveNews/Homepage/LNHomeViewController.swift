80689514b717ff3b569ba2758ce2eafdef2cc1fc
yingongzheng <yingongzheng@126.com>
2018-04-23 17:23:00 +0800

    alamofire 请求 Masonry布局控件

--- LoveNews/LoveNews/Homepage/LNHomeViewController.swift
+++ LoveNews/LoveNews/Homepage/LNHomeViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class LNHomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
-    let  arry:[String] = ["我是谁", "我从哪里来", "要到哪里去"]
+    var dataArr:Array<MovieModel>?
     lazy var table:UITableView  = {
         let tempTableView = UITableView (frame: self.view.frame)
         tempTableView.delegate = self
@@ -25,12 +26,37 @@ class LNHomeViewController: UIViewController,UITableViewDelegate,UITableViewData
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        mockData()
         self.view.addSubview(table)
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        //getData()
+    }
+    
+    func mockData() {
+        let movieModel = MovieModel()
+        movieModel.img = "http://img5.mtime.cn/mt/2018/03/12/094135.36237108_1280X720X2.jpg"
+        movieModel.titleCn = "狂暴巨兽"
+        movieModel.actorName1 = "恩·强森"
+        movieModel.directorName = "布拉德·佩顿"
+        dataArr = [movieModel]
+
+    }
+    func getData() {
+        //参考https://raw.githubusercontent.com/jokermonn/-Api/master/Time.md
+        let requestUrl = "https://api-m.mtime.cn/PageSubArea/HotPlayMovies.api?locationId=290"
+        Alamofire.request(requestUrl).responseJSON { (response) in
+            //guard let守护一定有值。如果没有，直接返回。
+            guard let result = response.result.value else {
+                //选类型后面加一个感叹号（!）来表示它肯定有值。
+                print(response.result.error!)
+                return
+            }
+            let json = JSON(result)
+            print(json)
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arry.count
+        return dataArr!.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -38,24 +64,17 @@ class LNHomeViewController: UIViewController,UITableViewDelegate,UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = (table.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)) as UITableViewCell
-        cell.textLabel?.text = arry[indexPath.row]
+        let indentifier = "HomeListCell"
+        var cell:HomeListCell! = tableView.dequeueReusableCell(withIdentifier: indentifier)as?HomeListCell
+        if cell == nil {
+            cell = HomeListCell(style: .default, reuseIdentifier: indentifier)
+        }
+        cell.setValueForCell(model: dataArr![indexPath.row])
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         //测试网络请求
-         getRequest()
-        
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.00
     }
-
-    func getRequest() {
-       let parameters:Dictionary = ["key":"93c921ea8b0348af8e8e7a6a273c41bd"]
-       Alamofire.request( "http://apis.haoservice.com/weather/city", method: .get, parameters: parameters)
-        .responseJSON { response in
-            print("response=\(response)")
-            print("result=\(response.result)")
-            //解析
-        }
-     }
+    
 }
---
+++ LoveNews/LoveNews/Homepage/View/HomeListCell/HomeListCell.swift
@@ -0,0 +1,91 @@
+//
+//  HomeListCell.swift
+//  LoveNews
+//
+//  Created by yingz on 2018/4/23.
+//  Copyright © 2018年 ygz. All rights reserved.
+//
+
+import UIKit
+import Masonry
+import Alamofire
+
+class HomeListCell: UITableViewCell {
+
+    var coverImageView:UIImageView?
+    var titleCn:UILabel?
+    var actorNameLab:UILabel?
+    var directorNameLab:UILabel?
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        coverImageView = UIImageView()
+        self.addSubview(coverImageView!)
+        
+        titleCn = UILabel()
+        self.addSubview(titleCn!)
+
+        actorNameLab = UILabel()
+        self.addSubview(actorNameLab!)
+
+        directorNameLab = UILabel()
+        self.addSubview(directorNameLab!)
+        
+        makeConstraints()
+    
+    }
+    
+    func makeConstraints() {
+        
+        coverImageView!.mas_makeConstraints({ (make : MASConstraintMaker!) in
+            make.left.equalTo()(self.mas_left)?.offset()(0)
+            make.top.equalTo()(self.mas_top)?.offset()(0)
+            make.width.mas_equalTo()(100)
+            make.height.mas_equalTo()(100)
+        })
+        
+        titleCn!.mas_makeConstraints({ (make : MASConstraintMaker!) in
+            make.left.equalTo()(coverImageView?.mas_right)?.offset()(10)
+            make.top.equalTo()(self.mas_top)?.offset()(13)
+            make.width.mas_equalTo()(200)
+            make.height.mas_equalTo()(16)
+        })
+
+        actorNameLab!.mas_makeConstraints({ (make : MASConstraintMaker!) in
+            make.left.equalTo()(coverImageView?.mas_right)?.offset()(10)
+            make.top.equalTo()(titleCn?.mas_bottom)?.offset()(13)
+            make.width.mas_equalTo()(100)
+            make.height.mas_equalTo()(16)
+        })
+
+        directorNameLab!.mas_makeConstraints({ (make : MASConstraintMaker!) in
+            make.left.equalTo()(coverImageView?.mas_right)?.offset()(10)
+            make.top.equalTo()(actorNameLab?.mas_bottom)?.offset()(13)
+            make.width.mas_equalTo()(100)
+            make.height.mas_equalTo()(16)
+        })
+    }
+    
+    func setValueForCell(model:MovieModel){
+        
+        //定义URL对象
+        let url = URL(string: model.img!)
+        //从网络获取数据流
+        let data = try! Data(contentsOf: url!)
+        //通过数据流初始化图片
+        let newImage = UIImage(data: data)
+        self.coverImageView?.image = newImage
+        
+        
+        self.titleCn?.text = String(format: "%@%@","电影名:",model.titleCn!)
+        self.actorNameLab?.text = model.actorName1
+        self.directorNameLab?.text = model.directorName
+    }
+
+}
