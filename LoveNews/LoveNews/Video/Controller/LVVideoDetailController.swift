b595efded0dd1e681cab8390a7d66cc02955c7de
yingongzheng <yingongzheng@126.com>
2018-05-09 11:25:48 +0800

    废弃Player  改用VGPlayer

--- LoveNews/LoveNews/Video/Controller/LVVideoDetailController.swift
+++ LoveNews/LoveNews/Video/Controller/LVVideoDetailController.swift
@@ -12,81 +12,74 @@
 import UIKit
 import Alamofire
 import AlamofireObjectMapper
-import Player
+import VGPlayer
+import SnapKit
 
 
-class LVVideoDetailController :LNBaseViewController,PlayerDelegate,PlayerPlaybackDelegate{
-    
-    func playerCurrentTimeDidChange(_ player: Player) {
+class LVVideoDetailController :LNBaseViewController{
+   
 
-    }
-    
-    func playerReady(_ player: Player) {
-        
-    }
-    
-    func playerPlaybackWillStartFromBeginning(_ player: Player) {
-
-
-    }
-    
-    func playerPlaybackDidEnd(_ player: Player) {
-
-    }
-    
-    func playerPlaybackWillLoop(_ player: Player) {
-        
-    }
-    
-    func playerPlaybackStateDidChange(_ player: Player) {
-        
-    }
-    
-    func playerBufferingStateDidChange(_ player: Player) {
-        
-    }
-    
-    func playerBufferTimeDidChange(_ bufferTime: Double) {
-        
-    }
-    
-
-    var player:Player  = Player()
+    var player = VGPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        let url = URL(string: "https://vfx.mtime.cn/Video/2018/04/24/mp4/180424224506970938_480.mp4")!
+        self.player.replaceVideo(url)
+        view.addSubview(self.player.displayView)
         
-        self.player.playerDelegate = self
-        self.player.playbackDelegate = self
-        self.player.view.frame = self.view.bounds
-        
-        self.addChildViewController(self.player)
-        self.view.addSubview(self.player.view)
-        
-        
-        self.player.didMove(toParentViewController: self)
-
-        
-        let videoUrl: URL = URL(string: "https://vfx.mtime.cn/Video/2018/04/24/mp4/180424224506970938_480.mp4")!
-            self.player.url = videoUrl
-        
-         self.player.playFromBeginning()
+        self.player.play()
+        self.player.delegate = self
+        self.player.displayView.delegate = self
+        self.player.displayView.titleLabel.text = "China NO.1"
+        self.player.displayView.snp.makeConstraints { [weak self] (make) in
+            guard let strongSelf = self else { return }
+            make.top.equalTo(strongSelf.view.snp.top)
+            make.left.equalTo(strongSelf.view.snp.left)
+            make.right.equalTo(strongSelf.view.snp.right)
+            make.height.equalTo(strongSelf.view.snp.width).multipliedBy(3.0/4.0)
+        }
         
     }
     
-    
     override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
         self.navigationController?.tabBarController?.tabBar.isHidden = true
+        self.player.play()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
         self.navigationController?.tabBarController?.tabBar.isHidden = false
+        self.player.pause()
     }
-
     
-    
- 
+}
 
+extension LVVideoDetailController: VGPlayerDelegate {
+    func vgPlayer(_ player: VGPlayer, playerFailed error: VGPlayerError) {
+        print(error)
+    }
+    func vgPlayer(_ player: VGPlayer, stateDidChange state: VGPlayerState) {
+        print("player State ",state)
+    }
+    func vgPlayer(_ player: VGPlayer, bufferStateDidChange state: VGPlayerBufferstate) {
+        print("buffer State", state)
+    }
+    
+}
+
+extension LVVideoDetailController: VGPlayerViewDelegate {
+    
+    func vgPlayerView(_ playerView: VGPlayerView, willFullscreen fullscreen: Bool) {
+        
+    }
+    func vgPlayerView(didTappedClose playerView: VGPlayerView) {
+        if playerView.isFullScreen {
+            playerView.exitFullscreen()
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
 
 }
 
